e63f480445f996155fce815e6c944127ec4ce865
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-06-17 23:12:46 +0900

    2020-06-16 조건에 따라 테스트 실행하기

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -1,49 +1,30 @@
 package me.whiteship.inflearnthejavatest;
 
-import jdk.jfr.Enabled;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.time.Duration;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
-    @Test
+    //@RepeatedTest 반복 횟수와 반복 테스트 이름을 설정 할 수 있다.
+    // value = 반복할 횟수  currentRepetition = 현재 반복 횟수 totalRepetitions = 총 반복해야할 횟수
+    // repetitionInfo 타입의 인자를 받을 수 있다.
     @DisplayName("스터디 만들기")
-//    @EnabledOnOs({OS.MAC, OS.LINUX}) //OS환경에 따라 테스트를 실행 시킬 수 있음.
-//    @EnabledOnJre(JRE.JAVA_11) //  JRE버전에 따라 테스트를 실행 시킬 수 있음.
-//    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches="LOCAL")
-    void create_new_study() {          //junit 4 에서는 public이여야됨
-        String test_env = System.getenv("TEST_ENV");
-//        System.out.println(test_env);
-//        assumeTrue("LOCAL".equalsIgnoreCase(test_env));  //환경변수 TEST_ENV가 LOCAL이면 실행
-        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-            System.out.println("local");
-            Study actual = new Study(100);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        });
-        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-            System.out.println("yongsoo");
-            Study actual = new Study(10);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        });
-
-
-
+    @RepeatedTest(value = 10 ,name ="{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("test"+repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
     }
 
-    @BeforeEach //각각의 테스트가 실행된 이전에 실행
-    void beforeEach(){
-        System.out.println("beforeEach");
+    //@ParameterizedTest 테스트에 여러 다른 매개변수를 대입해가며 반복 실행한다.
+    // name = 테스트의 이름을 지정할 수있다.
+    // @ValueSource 테스트에서 사용할 매개변수를 지정
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가","많이","추워지고","있네요"})
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
-    @AfterEach
-    void afterEach(){ //각각의 테스트가 실행된 이후에 실행행
-        System.out.println("aterEach");
-    }
+
 }
