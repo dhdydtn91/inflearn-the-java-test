5bc2cfd993d6401534ea281432099a53127acf21
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-06-18 00:51:45 +0900

    2020-06-17 테스트 인스턴스 설정

--- src/main/java/me/whiteship/inflearnthejavatest/Study.java
+++ src/main/java/me/whiteship/inflearnthejavatest/Study.java
@@ -6,17 +6,33 @@ public class Study {
 
     int limit ;
 
+    String name;
+
     public Study(int limit){
         if(limit < 0 ){
             throw new IllegalArgumentException("limit은 0보다 커야한다.");
         }
         this.limit = limit;
     }
+
     public StudyStatus getStatus() {
         return this.status;
     }
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
